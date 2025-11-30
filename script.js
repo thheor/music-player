274b78152a06f69642a67d54a736fa5b63302484
Theo <prstylukman@gmail.com>
2025-11-30 23:46:07 +0700

    Finish the landing page

---
+++ script.js
@@ -0,0 +1,5 @@
+const title = document.getElementById('title'),
+      artist = document.getElementById('artist'),
+      prevBtn = document.getElementById('prev'),
+      playBtn = document.getElementById('play'),
+      nextBtn = document.getElementById('next');
