89cb1406a539f39103a44967aefc4223e9079320
Theo <prstylukman@gmail.com>
2025-12-03 10:53:29 +0700

    add music progress

--- script.js
+++ script.js
@@ -2,13 +2,18 @@ const title1 = document.getElementById('title-1'),
       title2 = document.getElementById('title-2'),
       title3 = document.getElementById('title-3'),
       artist = document.getElementById('play-artist'),
+      dur = document.getElementById('dur'),
       prevBtn = document.getElementById('prev'),
       playBtn = document.getElementById('play'),
       nextBtn = document.getElementById('next'),
       cover = document.getElementById('cover'),
       playMsc = document.getElementById('music-play'),
       footer = document.getElementById('footer'),
-      playTitle = document.getElementById('play-title');
+      playTitle = document.getElementById('play-title'),
+      currentTimeEl = document.getElementById('current-time'),
+      durationEl = document.getElementById('duration'),
+      progress = document.getElementById('progress'),
+      playerProgress = document.getElementById('player-progress');
 
 let isPlaying = false;
 let musicIndex = 0;
@@ -35,8 +40,6 @@ const songs = [
         cover: 'src/song_img/3.jpg'
     }
 ]
-
-// title.addEventListener("click", changeCover(songs));
 
 function togglePlay(){
     if(isPlaying){
@@ -69,7 +72,6 @@ function pauseSong(){
 }
 
 function loadMusic(index){
-    // musicIndex = index;
     music.src = songs[index].path;
     cover.src = songs[index].cover;
     playTitle.innerHTML = songs[index].title;
@@ -86,12 +88,28 @@ function displayMusic() {
     playMsc.style.display = 'initial';
 }
 
+function updateProgressBar() {
+    const { duration, currentTime } = music;
+    const progressPercent = (currentTime / duration) * 100;
+    progress.style.width = `${progressPercent}%`;
+
+    const formatTime = (time) => String(Math.floor(time)).padStart(2, '0');
+    durationEl.textContent = `${formatTime(duration / 60)}:${formatTime(duration % 60)}`;
+    currentTimeEl.textContent = `${formatTime(currentTime / 60)}:${formatTime(currentTime % 60)}`;
+}
+
+function setProgressBar(e) {
+    const width = playerProgress.clientWidth;
+    const clickX = e.offsetX;
+    music.currentTime = (clickX / width) * music.duration;
+}
+
 title1.addEventListener("click", () => topMusic(0));
 title2.addEventListener("click", () => topMusic(1));
 title3.addEventListener("click", () => topMusic(2));
 playBtn.addEventListener("click", () => togglePlay(songs[1]));
 prevBtn.addEventListener("click", () => changeMusic(-1));
 nextBtn.addEventListener("click", () => changeMusic(1));
-// window.addEventListener('keydown', music.pause());
-// prevBtn.addEventListener("click", () => changeMusic(-1));
-console.log(musicIndex);
+music.addEventListener('ended', () => changeMusic(1));
+music.addEventListener('timeupdate', updateProgressBar);
+playerProgress.addEventListener('click', setProgressBar);
